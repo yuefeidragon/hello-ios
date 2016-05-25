3a7506f9bbe51e9586e868b6f7522d1471584e21
yuefeidragon <275853207@qq.com>
2016-05-25 15:50:46 +0800

    新建了一个项目

---
+++ 蓝鸥岳飞test/蓝鸥岳飞test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蓝鸥岳飞test
+//
+//  Created by 岳飞 on 16/5/25.
+//  Copyright © 2016年 岳飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 蓝鸥岳飞test/蓝鸥岳飞test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  蓝鸥岳飞test
+//
+//  Created by 岳飞 on 16/5/25.
+//  Copyright © 2016年 岳飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
